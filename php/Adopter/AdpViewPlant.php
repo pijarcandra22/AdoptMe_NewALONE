cdb2ab03f19ad4306e1fe5d295b03ba10c85d5a5
pijarcandra22 <pijarcandra22@gmail.com>
2022-03-30 13:37:05 +0700

    Memperbaiki kemunculan data tanaman

--- php/Adopter/AdpViewPlant.php
+++ php/Adopter/AdpViewPlant.php
@@ -2,7 +2,7 @@
     include dirname( dirname(__DIR__ . PHP_EOL) . PHP_EOL)."\php\GlobalFun.php";
     global $conn;
     // TODO : INNER JOIN (Kondisi Terakhir (inner join tb_data_perawatan)
-    $sql = "SELECT ta.*, pe.nama_pengelola FROM `tb_tanaman` AS ta LEFT JOIN `tb_pengelola` AS pe USING (id_pengelola) WHERE ta.`id_adopter`=$id AND ta.`status` = 'waiting' AND ta.nama_tanaman = '$namaTanaman'";
+    $sql = "SELECT ta.*, pe.nama_pengelola FROM `tb_tanaman` AS ta LEFT JOIN `tb_pengelola` AS pe USING (id_pengelola) WHERE ta.`status` = '' GROUP BY ta.nama_tanaman";
     $readTable = mysqli_query($conn, $sql);
     $rows = array();
     while ($getTableData = mysqli_fetch_assoc( $readTable )) $rows[]=$getTableData;
