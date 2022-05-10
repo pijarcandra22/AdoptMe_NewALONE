55e7377ec2d70cf9c8a40d9085134c4a2766557d
pijarcandra22 <pijarcandra22@gmail.com>
2022-05-10 13:53:53 +0700

    Update untuk menampilkan tanaman yang sudah ter adopt

--- php/Adopter/AdpViewPlant.php
+++ php/Adopter/AdpViewPlant.php
@@ -2,7 +2,9 @@
     include "../GlobalFun.php";
     global $conn;
     // TODO : INNER JOIN (Kondisi Terakhir (inner join tb_data_perawatan)
-    $sql = "SELECT ta.*, pe.nama_pengelola FROM `tb_tanaman` AS ta LEFT JOIN `tb_pengelola` AS pe USING (id_pengelola) GROUP BY ta.nama_tanaman";
+    $sql = "SELECT COUNT(CASE WHEN ta.status='' THEN 1 END) as jumlah_tanaman,ta.*, pe.nama_pengelola 
+    FROM `tb_tanaman` AS ta LEFT JOIN `tb_pengelola` AS pe USING (id_pengelola) GROUP BY 
+    ta.nama_tanaman;";
     $readTable = mysqli_query($conn, $sql);
     $rows = array();
     while ($getTableData = mysqli_fetch_assoc( $readTable )) $rows[]=$getTableData;
