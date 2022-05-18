8c5ecd2c7777bbd98de4b6fa2e6492871a60cab6
WayanPande <yande554@gmail.com>
2022-05-18 14:02:35 +0800

    compress upload image to webp

--- php/GlobalFun.php
+++ php/GlobalFun.php
@@ -1,12 +1,14 @@
-<?php 
+<?php
 
-$conn = mysqli_connect("localhost", "root", "", "adopt_me"); 
+$conn = mysqli_connect("localhost", "root", "", "adopt_me");
 
-function tes() {
+function tes()
+{
     echo "masuk";
 }
 
-function closeAndDirect($redirect, $respon) {
+function closeAndDirect($redirect, $respon)
+{
     global $conn;
     $conn->close();
 
@@ -14,27 +16,46 @@ function closeAndDirect($redirect, $respon) {
     exit;
 }
 
-function uploadImage($gambar,$location){
-    $extgambarvalid = ['jpg','jpeg','png'];
-    $_SESSION['error_content']='';
+function uploadImage($gambar, $location)
+{
+    $extgambarvalid = ['jpg', 'jpeg', 'png'];
+    $_SESSION['error_content'] = '';
     $namafile = $_FILES[$gambar]["name"];
     $ukuranfile = $_FILES[$gambar]["size"];
     $tmpName = $_FILES[$gambar]["tmp_name"];
     $error = $_FILES[$gambar]["error"];
 
-    if($error === 4){
-        echo "1"; return false;
+    if ($error === 4) {
+        echo "1";
+        return false;
     }
-    $extgambar = explode('.',$namafile);
+    $extgambar = explode('.', $namafile);
     $extgambar = strtolower(end($extgambar));
-    if(!in_array($extgambar,$extgambarvalid)){
-        $_SESSION['error_content']='Gambar Tidak Vailid';
-        echo "2"; return false;
+    if (!in_array($extgambar, $extgambarvalid)) {
+        $_SESSION['error_content'] = 'Gambar Tidak Vailid';
+        echo "2";
+        return false;
     }
+
+    //Create an image object.
+    if ($extgambar === "jpg") {
+        $im = imagecreatefromjpeg($tmpName);
+    } else if ($extgambar === "png") {
+        $im = imagecreatefrompng($tmpName);
+    } else {
+        $im = imagecreatefromjpeg($tmpName);
+    }
+
+    // check file size
+    if ($ukuranfile > 2048) {
+        $compress = 60;
+    } else {
+        $compress = 80;
+    }
+
+
     $namafilebBaru = uniqid();
-    $namafilebBaru .= ".".$extgambar;
-    move_uploaded_file($tmpName,$location.$namafilebBaru);
+    $namafilebBaru .= ".webp";
+    imagewebp($im, $location . $namafilebBaru, $compress);
     return $namafilebBaru;
 }
-
-?>
